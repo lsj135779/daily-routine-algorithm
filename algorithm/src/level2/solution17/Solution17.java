d6164b45614e7f29de75aa7b9dfa1bf2c6d6a9e2
이승준 <lsj135779@gmail.com>
2023-11-14 11:14:14 +0900

    level2 17번 문제

---
+++ algorithm/src/level2/solution17/Solution17.java
@@ -0,0 +1,15 @@
+package level2.solution17;
+
+public class Solution17 {
+    public int[] solution(long n) {
+        // n을 string 으로 바꿔서 length 를 이용해 길이를 구할 수 있음.
+        int[] answer = new int[(int)Math.log10(n)+1]; // n의 길이 구하기
+        int idx = 0;
+        while (n >= 1) {
+            answer[idx] = (int)(n % 10); // int 형으로 변환
+            idx++;
+            n = n / 10;
+        }
+        return answer;
+    }
+}
