41700c8c64fd822e012d9060e358755381407d40
이승준 <lsj135779@gmail.com>
2023-11-27 22:58:48 +0900

    level2 32번 문제

---
+++ algorithm/src/level2/solution32/Solution32.java
@@ -0,0 +1,11 @@
+package level2.solution32;
+
+public class Solution32 {
+    public int solution(int[] a, int[] b) {
+        int answer = 0;
+        for (int i = 0; i < a.length; i++) {
+            answer += a[i] * b[i];
+        }
+        return answer;
+    }
+}
